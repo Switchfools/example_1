7dbe7932e17e3fd8ecfc0ecbc7ae0bbc45b9c5ba
Nicolás Vergara <37335567+Switchfools@users.noreply.github.com>
2022-09-11 22:49:05 -0500

    backbone

--- api-demo/app.py
+++ api-demo/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, make_response,request
 from flask_cors import CORS, cross_origin
+
 import modules.hello_world as hello_world
+import modules.operations as operations
+import modules.formating as formating
 
 from dotenv import load_dotenv, find_dotenv
 
@@ -15,3 +18,15 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 @cross_origin()
 def return_hello_world():
     return hello_world.hello_world()
+
+
+@app.route('/api/operations/multiplicate/<parameters>', methods=['GET'])
+@cross_origin()
+def return_multiplication(parameters):
+    numbers=parameters.split(',')
+    return operations.multiplicate(numbers)
+
+@app.route('/api/formating/lowercase_string/<string>', methods=['GET'])
+@cross_origin()
+def return_lowercase_string(string):
+    return formating.lowercase_string(string)
