5deb314cb338973cf7cdc974a6a79d9be5da7b63
Nicolás Vergara <37335567+Switchfools@users.noreply.github.com>
2022-09-11 23:16:58 -0500

    adding formating

--- api-demo/modules/formating/__init__.py
+++ api-demo/modules/formating/__init__.py
@@ -1,8 +1,6 @@
-from urllib import response
-import orjson
 from flask import Response
 
 
-def hello_world():
-    return Response("hello world", mimetype='application/json')
+def lowercase_string(string):
+    return Response(string.lower(), mimetype='application/json')
 
